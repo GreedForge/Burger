7511bbddc90c5c5fe7fb77d3400e8c1c2abb8e92
GreedForge <jafecafe@gmail.com>
2017-08-08 17:54:26 -0500

    i think i replaced what needed to be replaced

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -3,13 +3,13 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-var cat = require("../models/cat.js");
+var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  cat.all(function(data) {
+  burger.all(function(data) {
     var hbsObject = {
-      cats: data
+      burgers: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
@@ -17,10 +17,10 @@ router.get("/", function(req, res) {
 });
 
 router.post("/", function(req, res) {
-  cat.create([
-    "name", "sleepy"
+  burger.create([
+    "name", "devoured"
   ], [
-    req.body.name, req.body.sleepy
+    req.body.name, false
   ], function() {
     res.redirect("/");
   });
@@ -31,20 +31,20 @@ router.put("/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  cat.update({
-    sleepy: req.body.sleepy
+  burger.update({
+    devoured: true
   }, condition, function() {
     res.redirect("/");
   });
 });
 
-router.delete("/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+// router.delete("/:id", function(req, res) {
+//   var condition = "id = " + req.params.id;
 
-  cat.delete(condition, function() {
-    res.redirect("/");
-  });
-});
+//   cat.delete(condition, function() {
+//     res.redirect("/");
+//   });
+// });
 
 // Export routes for server.js to use.
 module.exports = router;
--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,16 @@
 ### Schema
 
-CREATE DATABASE cat_db;
-USE cat_db;
+CREATE DATABASE burger_db;
+USE burger_db;
 
-CREATE TABLE cats
+CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	sleepy BOOLEAN DEFAULT false,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	date TIMESTAMP,
 	PRIMARY KEY (id)
 );
+
+
+select*from burgers;
