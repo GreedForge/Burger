6420c767715c24c379a59f329d21e3508e20e2e3
GreedForge <jafecafe@gmail.com>
2017-09-19 12:00:44 -0500

    beautified code

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -7,37 +7,37 @@ var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  burger.all(function(data) {
-    var hbsObject = {
-      burgers: data
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
-  });
+    burger.all(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
 });
 
 router.post("/", function(req, res) {
-  var time =  Date.now();
-  console.log(req.body.name);
-  burger.create([
-    "burger_name", "devoured", "date"
-  ], [
-    req.body.name, false, "2008-01-01 00:00:01"
-  ], function() {
-    res.redirect("/");
-  });
+    var time = Date.now();
+    console.log(req.body.name);
+    burger.create([
+        "burger_name", "devoured", "date"
+    ], [
+        req.body.name, false, "2008-01-01 00:00:01"
+    ], function() {
+        res.redirect("/");
+    });
 });
 
 router.put("/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+    var condition = "id = " + req.params.id;
 
-  console.log("condition", condition);
+    console.log("condition", condition);
 
-  burger.update({
-    devoured: true
-  }, condition, function() {
-    res.redirect("/");
-  });
+    burger.update({
+        devoured: true
+    }, condition, function() {
+        res.redirect("/");
+    });
 });
 
 // router.delete("/:id", function(req, res) {
